db98ed5eb448c4c0a7e82adfac4fbc3ed5da9204
Rohit saxena <75602589+Rohit-saxena125@users.noreply.github.com>
2022-07-07 14:22:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const pdfparse = require("pdf-parse");
 const app = express();
 app.use("/", express.static("public"));
 app.use(fileUpload());
+
 app.post("/extract-text", (req, res) => {
+    try{
   if (req.files.pdfFile === null) {
     return res.status(400).send("No file uploaded");
   }
@@ -22,7 +24,7 @@ app.post("/extract-text", (req, res) => {
         let pay= "";
         for (var i = 0; i < lines.length; i++) 
         {
-          if ((lines[i].match(/\d{2}-\d{2}-\d{4}/g) ||lines[i].match(/(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?),+(\d{1,2}),+(\d{4})/g)||lines[i].match(/\d{2}\/\d{2}\/\d{4}/g))||(lines[i].match(/\d{2}-\d{1}-\d{4}/g)||lines[i].match(/\d{2}\/\d{1}\/\d{4}/g)))
+          if ((lines[i].match(/\d{1,2}-\d{1,2}-\d{4}/g) ||lines[i].match(/(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?),+(\d{1,2}),+(\d{4})/g)||lines[i].match(/\d{1,2}\/\d{1,2}\/\d{4}/g)))
           {
             newText += lines[i] + "\n";  
           } 
@@ -30,7 +32,7 @@ app.post("/extract-text", (req, res) => {
         fs.appendFileSync("public/text.txt", newText);
         for(var j=0;j<lines.length;j++)
         {
-            if(lines[j].match(/paid/g)||lines[j].match(/recieved/g)||lines[j].match(/donated/g)||lines[j].match(/debited/g)||lines[j].match(/credited/g))
+            if(lines[j].match(/Rs/g)||lines[j].match(/paid/g)||lines[j].match(/recieved/g)||lines[j].match(/donated/g)||lines[j].match(/debited/g)||lines[j].match(/credited/g))
             {
                 pay +=lines[j] + "\n";
             }
@@ -38,12 +40,17 @@ app.post("/extract-text", (req, res) => {
         fs.appendFileSync("public/payment.txt", pay);
 
 
-            console.log(newText); 
+            console.log(newText);
             console.log(pay);
             res.send("Date"+"\n"+newText+"\n"+"Payments"+"\n"+pay);
             
       }); //.then()     //delete file
     }
   );
+}
+
+catch{
+    res.status(500).send("Not Found")
+  }
 });
 app.listen(4000);
